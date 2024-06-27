b74cc314bc6d74f4175fb58a764b67675eb109a1
Talitrus <9120001+Talitrus@users.noreply.github.com>
2024-06-27 12:57:49 -0700

    fix doc typo, add result_from function

---
+++ R/result_from.R
@@ -0,0 +1,28 @@
+#' Take a Hamilton input file and return the resulting destination plate.
+#'
+#'
+#' @importFrom dplyr mutate
+#' @importFrom magrittr %>%
+#' @param data A tibble or input file with columns SampleID, PlateID, WellID, and DestWellID.
+
+#'
+#' @return Returns a tibble with columns SampleID, PlateID, and WellID.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' data <- tibble::tibble(
+#'   SampleID = c("C234323CVP", "C240026CVP", "C240036CVP", "C240199CVP"),
+#'   PlateID = c("CVP20240204CDFW01", "CVP20240204CDFW01", "CVP20240204CDFW01", "CVP20240218CDFW02"),
+#'   WellID = c("A1", "A2", "A3","A1"),
+#'   DestWellID = c("A1", "A2", "A3","A4")
+#' )
+#' result_plate <- result_from(data)
+#' result_plate
+#' }
+result_from <- function() {
+  result_plate <- data %>%
+    dplyr::mutate(WellID = DestWellID) %>%
+    dplyr::select(SampleID, PlateID, WellID)
+  return(result_plate)
+}
